f5b64f1e19bc5ef881fb20c8ef02166670b211fe
Corey Wallis <corey.wallis@flinders.edu.au>
2019-08-09 09:39:20 +0930

    RPA-159 Add first version of metadata class

---
+++ app/WorkflowMetadata.js
@@ -0,0 +1,140 @@
+import * as path from "path";
+
+/**
+ * Store the documentation extracted from a XAML file.
+ */
+export class WorkflowMetadata {
+
+  /**
+   * Construct a new object.
+   *
+   * @param {string} filePath Path to the root directory of the UiPath project.
+   * @throws {TypeError} Parameter projectPath is required and must be a string.
+   *
+   * @since 1.0.0
+   */
+  constructor( filePath ) {
+
+    if ( !filePath || typeof filePath !== "string" ) {
+      throw new TypeError( "filePath parameter is required and must be a string" );
+    }
+
+    this.filePath = filePath;
+
+    this.validArgumentDirections = [
+      "in",
+      "out",
+      "in/out"
+    ];
+
+    this.arguments = new Map();
+
+  }
+
+  /**
+   * Get the path to the XAML file.
+   *
+   * @returns {string} The full path to the XAML file.
+   *
+   * @since 1.0.0
+   */
+  getFilePath() {
+    return this.filePath;
+  }
+
+  /**
+   * Get the name of the XAML file.
+   *
+   * @returns {string} The file name component of the path to the XAML file.
+   *
+   * @since 1.0.0
+   */
+  getFileName() {
+    return path.basename( this.filePath );
+  }
+
+  /**
+   * Add the name of the workflow to the metadata.
+   *
+   * @param {string} workflowName The name of the workflow.
+   *
+   * @since 1.0.0
+   */
+  setWorkflowName( workflowName ) {
+    if ( !workflowName || typeof workflowName !== "string" ) {
+      throw new TypeError( "workflowName parameter is required and must be a string" );
+    }
+
+    this.workflowName = workflowName;
+  }
+
+  /**
+   * Return the name of the workflow.
+   *
+   * @returns {string} The name of the workflow.
+   * @throws {ReferenceError} If the workflow name has not been set.
+   * @since 1.0.0
+   */
+  getWorkflowName() {
+    if ( typeof this.workflowName === "undefined" ) {
+      throw new ReferenceError( "The workflowName property has not been set." );
+    }
+    return this.workflowName;
+  }
+
+  /**
+   * Add an argument to the list of arguments defined in the workflow.
+   *
+   * @param {string} name The name of the argument.
+   * @param {string} direction The direction of the argument (in|out|in/out).
+   * @param {string} type The data type of the argument.
+   * @param {string} annotation The annotation describing the argument.
+   * @param {string} defaultValue The default value.
+   */
+  addArgument( name, direction, type, annotation, defaultValue ) {
+
+    if ( !name || typeof name !== "string" ) {
+      throw new TypeError( "name parameter is required and must be a string" );
+    }
+
+    if ( !direction || typeof direction !== "string" ) {
+      throw new TypeError( "direction parameter is required and must be a string" );
+    }
+
+    if ( !this.validArgumentDirections.includes( direction ) ) {
+      throw new TypeError(
+        "direction parameter must be one of " + this.validArgumentDirections.join( "|" )
+      );
+    }
+
+    if ( !type || typeof type !== "string" ) {
+      throw new TypeError( "type parameter is required and must be a string" );
+    }
+
+    if ( typeof annotation !== "string" ) {
+      throw new TypeError( "annotation parameter is required and must be a string" );
+    }
+
+    if ( typeof defaultValue !== "string" ) {
+      throw new TypeError( "defaultValue parameter is required and must be a string" );
+    }
+
+    this.arguments.set( name, {
+      "name": name,
+      "direction": direction,
+      "type": type,
+      "annotation": annotation,
+      "defaultValue": defaultValue
+    } );
+  }
+
+  /**
+   * Get the list of arguments.
+   *
+   * @returns {Map} A map of argument objects.
+   */
+  getArguments() {
+    return this.arguments;
+  }
+
+}
---
+++ test/TestWorkflowMetadata.js
@@ -0,0 +1,295 @@
+import { WorkflowMetadata } from "../app/WorkflowMetadata.js";
+
+import * as assert from "assert";
+
+/**
+ * Test the Xamlmetadataation object.
+ */
+describe( "WorkflowMetadata", function() {
+
+  /**
+   * Test constructing a new instance of the class.
+   */
+  describe( "#constructor", function() {
+    it( "should throw an error if the parameter is not supplied", function() {
+      assert.throws( function() {
+        new WorkflowMetadata();
+
+      }, TypeError );
+    } );
+
+    it( "should throw an error if the parameter is not a string", function() {
+      assert.throws( function() {
+        new WorkflowMetadata( new Object() );
+      }, TypeError );
+    } );
+
+    it( "should throw an error if the parameter is an empty string", function() {
+      assert.throws( function() {
+        new WorkflowMetadata( "" );
+      }, TypeError );
+    } );
+
+    it( "should return an object of the right type", function() {
+      let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+
+      assert.ok(
+        metadata instanceof  WorkflowMetadata
+      );
+    } );
+  } );
+
+  /**
+   * Test getting the file path.
+   */
+  describe( "#getFilePath", function() {
+    it( "should return a string", function() {
+      let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+
+      assert.strictEqual( typeof metadata.getFilePath(), "string" );
+    } );
+
+    it( "should return a string matching what was supplied to the constructor", function() {
+      let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+
+      assert.strictEqual( metadata.getFilePath(), "./test/artefacts/uno.xaml" );
+    } );
+  } );
+
+  /**
+   * Test getting the file name.
+   */
+  describe( "#getFileName", function() {
+    it( "should return a string", function() {
+      let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+
+      assert.strictEqual( typeof metadata.getFileName(), "string" );
+    } );
+
+    it( "should return a string matching the file supplied to the constructor", function() {
+      let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+
+      assert.strictEqual( metadata.getFileName(), "uno.xaml" );
+    } );
+  } );
+
+  /**
+   * Test setting the name of the workflow.
+   */
+  describe( "#setWorkflowName", function() {
+    it( "should throw an error if the parameter is not supplied", function() {
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.setWorkflowName();
+
+      }, TypeError );
+    } );
+
+    it( "should throw an error if the parameter is not a string", function() {
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.setWorkflowName( new Object() );
+      }, TypeError );
+    } );
+
+    it( "should throw an error if the parameter is an empty string", function() {
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.setWorkflowName( "" );
+      }, TypeError );
+    } );
+  } );
+
+  /**
+   * Test getting the name of the workflow.
+   */
+  describe( "#getWorkflowName", function() {
+    it( "should throw an error if the workflow name has not been set", function() {
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.getWorkflowName();
+      }, ReferenceError );
+    } );
+
+    it( "should return the same workflow name that is set", function() {
+      let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+      const testWorkflowName = "This is a test workflow name.";
+      metadata.setWorkflowName( testWorkflowName );
+      assert.strictEqual( testWorkflowName, metadata.getWorkflowName() );
+    } );
+  } );
+
+  /**
+   * Test adding an argument.
+   */
+  describe( "#addArgument", function() {
+    it( "should throw an error if the name parameter is not provided", function() {
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument();
+      }, TypeError );
+
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument();
+      }, /^TypeError: name/ );
+
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "" );
+      }, /^TypeError: name/ );
+    } );
+
+    it( "should throw an error if the direction parameter is not provided", function() {
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name" );
+      }, TypeError );
+
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name" );
+      }, /^TypeError: direction/ );
+
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", new Object() );
+      }, /^TypeError: direction/ );
+    } );
+
+    it( "should throw an error if the direction parameter is invalid", function() {
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "both" );
+      }, TypeError );
+
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "both" );
+      }, /^TypeError: direction parameter must be one of/ );
+
+
+      assert.doesNotThrow( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in", "type", "annotation", "defaultValue" );
+      }, TypeError );
+
+      assert.doesNotThrow( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "out", "type", "annotation", "defaultValue" );
+      }, TypeError );
+
+      assert.doesNotThrow( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in/out", "type", "annotation", "defaultValue" );
+      }, TypeError );
+    } );
+
+    it( "should throw an error if the type parameter is not provided", function() {
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in" );
+      }, TypeError );
+
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in" );
+      }, /^TypeError: type/ );
+
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in", new Object() );
+      }, /^TypeError: type/ );
+    } );
+
+    it( "should throw an error if the annotation parameter is not provided", function() {
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in", "string" );
+      }, TypeError );
+
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in", "string" );
+      }, /^TypeError: annotation/ );
+
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in", "string", new Object() );
+      }, /^TypeError: annotation/ );
+
+      assert.doesNotThrow( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in", "string", "", "" );
+      }, /^TypeError: annotation/ );
+    } );
+
+    it( "should throw an error if the defaultValue parameter is not provided", function() {
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in", "string", "" );
+      }, TypeError );
+
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in", "string", "" );
+      }, /^TypeError: defaultValue/ );
+
+      assert.throws( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in", "string", "", new Object() );
+      }, /^TypeError: defaultValue/ );
+
+      assert.doesNotThrow( function() {
+        let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+        metadata.addArgument( "Name", "in", "string", "", "", "" );
+      }, /^TypeError: defaultValue/ );
+    } );
+
+    it( "should store the new argument", function() {
+      let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+      metadata.addArgument( "testArgument", "in", "string", "An argument for testing", "Hello World!" );
+
+      assert.strictEqual( metadata.arguments.size, 1 );
+
+      assert.deepStrictEqual( metadata.arguments.get( "testArgument" ), {
+        "name": "testArgument",
+        "direction": "in",
+        "type": "string",
+        "annotation": "An argument for testing",
+        "defaultValue": "Hello World!"
+      } );
+    } );
+  } );
+
+  /**
+   * Test adding an argument.
+   */
+  describe( "#getArguments", function() {
+    it( "should return an empty map by default", function() {
+      let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+      let workflowArguments = metadata.getArguments();
+
+      assert.ok( workflowArguments instanceof Map );
+      assert.strictEqual( workflowArguments.size, 0 );
+
+    } );
+
+    it( "should return a map of arguments that were added", function() {
+      let metadata = new WorkflowMetadata( "./test/artefacts/uno.xaml" );
+      metadata.addArgument( "testArgument", "in", "string", "An argument for testing", "Hello World!" );
+      let workflowArguments = metadata.getArguments();
+
+      assert.ok( workflowArguments instanceof Map );
+      assert.strictEqual( workflowArguments.size, 1 );
+
+      assert.deepStrictEqual( workflowArguments.get( "testArgument" ), {
+        "name": "testArgument",
+        "direction": "in",
+        "type": "string",
+        "annotation": "An argument for testing",
+        "defaultValue": "Hello World!"
+      } );
+    } );
+  } );
+} );
