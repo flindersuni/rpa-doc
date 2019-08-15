flindersuni/rpa-doc

commit 1be2e1286e164c21ec80075735f0b44c2e33c7d7

RPA-157 Simplify getting list of xaml files in a project

app/UiPathProject.js
@@ -1,5 +1,6 @@
 import * as path from "path";
 import * as fs from "fs";
+import * as glob from "glob";
 
 /**
  * Represents the information contained in the UiPath project.json file.
@@ -142,40 +143,14 @@ export class UiPathProject {
    */
   getXamlFiles( recursive = false, publicOnly = false ) {
 
-    // Get a list of files in the target directory.
-    let fileList = [];
+    let globPattern = path.join( this.getProjectPath(), "*.xaml" );
 
-    if ( recursive === false ) {
-
-      // Only get the top level workflows.
-
-      fileList = fs.readdirSync( this.projectPath ); // eslint-disable-line security/detect-non-literal-fs-filename
-
-      //fileList = fileList.map( f => path.join( this.getProjectPath(), f ) );
-    } else {
-
-      // Get all workflows.
-
-      let tmp = [ this.projectPath ];
-
-      do {
-        let filePath = tmp.pop();
-        let fileStat = fs.lstatSync( filePath ); // eslint-disable-line security/detect-non-literal-fs-filename
-
-        if ( fileStat.isDirectory() ) {
-          fs // eslint-disable-line security/detect-non-literal-fs-filename
-            .readdirSync( filePath )
-            .forEach( f => tmp.push( path.join( filePath, f ) ) );
-        } else if ( fileStat.isFile() ) {
-          fileList.push( filePath );
-        }
-      } while ( tmp.length !== 0 );
+    if ( recursive ) {
+      globPattern = path.join( this.getProjectPath(), "**/*.xaml" );
     }
 
-    // Filter the list of files to only XAML files.
-    fileList = fileList.filter( function( element ) {
-      return path.extname( element ) === ".xaml" && path.basename( element ).startsWith( "~" ) === false;
-    } );
+    // Get a list of files in the target directory.
+    let fileList = glob.sync( globPattern );
 
     // Only return XAML files for public workflows if required.
     if ( publicOnly === true ) {

commit 0b67956c59bf96f04ee375c140dcd1d4ee73b277

RPA-157 Fix file path comparison bug
Fix a bug caused by 1be2e1286e164c21ec80075735f0b44c2e33c7d7

app/UiPathProject.js
@@ -152,13 +152,22 @@ export class UiPathProject {
     // Get a list of files in the target directory.
     let fileList = glob.sync( globPattern );
 
+    // Filter the list of files to exclude temporary files
+    fileList = fileList.filter( function( element ) {
+      return path.basename( element ).startsWith( "~" ) === false;
+    } );
+
+    fileList.map( function( filePath ) {
+      return path.posix.normalize( filePath );
+    } );
+
     // Only return XAML files for public workflows if required.
     if ( publicOnly === true ) {
       let privateWorkflows = this.getPrivateWorkflows();
 
       // Normalise the private workflow paths, enables cross platform comparison below.
       privateWorkflows = privateWorkflows.map( function( workflowPath ) {
-        return path.normalize( workflowPath );
+        return path.posix.normalize( workflowPath );
       } );
 
       privateWorkflows.forEach( function( privateWorkflow ) {